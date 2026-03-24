570bd6f09c41120ef5e8c2affa117f8348a39e55
NandoLFC12 <luizfernandolufe@yahoo.com.br>
2026-03-24 19:54:57 -0300

    Ajeitando

---
+++ script.js
@@ -0,0 +1,11 @@
+function converter() {
+            var decimal = Number(demo1.value);
+
+            demo2.innerHTML = decimal.toString(8);
+            demo3.innerHTML = decimal.toString(16);
+            demo4.innerHTML = decimal.toString(2);
+
+            
+
+     document.getElementById("cachorro_sabio").style.display = "block";
+}
